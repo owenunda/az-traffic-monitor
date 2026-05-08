21a289c7d5f6ad6210dc7f57c6f27b9796892171
owenunda <oweenunda.dev@gmail.com>
2026-05-08 01:58:49 -0500

    Refactor traffic data simulation to use real Medellín coordinates

--- simulador.js
+++ simulador.js
@@ -12,13 +12,32 @@ if (!connectionString) {
 
 const client = DeviceClient.fromConnectionString(connectionString, Mqtt);
 
+// Tus puntos reales de Medellín
+const puntosCongestionMedellin = [
+  { latitud: 6.2889, longitud: -75.5647 },
+  { latitud: 6.2108, longitud: -75.5735 },
+  { latitud: 6.2764, longitud: -75.5702 },
+  { latitud: 6.2081, longitud: -75.5653 },
+  { latitud: 6.2476, longitud: -75.5689 },
+  { latitud: 6.2452, longitud: -75.5774 },
+  { latitud: 6.2301, longitud: -75.5983 },
+  { latitud: 6.2448, longitud: -75.5941 },
+  { latitud: 6.2245, longitud: -75.6017 },
+  { latitud: 6.2519, longitud: -75.5698 }
+];
+
+
 async function sendTrafficData() {
-const velocidad = Math.floor(Math.random() * (15 - 5) + 5);
-const datos = JSON.stringify({
+  const puntoAleatorio = puntosCongestionMedellin[Math.floor(Math.random() * puntosCongestionMedellin.length)];
+  const velocidad = Math.floor(Math.random() * (15 - 5) + 5);
+  const datos = JSON.stringify({
     sensorId: 'sensor-tdea-01',
     velocidad: velocidad,
     // 6.279681, -75.583859
-    location: { lat: 6.279681, lng: -75.583859 },
+    location: {
+      lat: puntoAleatorio.latitud,
+      lng: puntoAleatorio.longitud
+    },
     timestamp: new Date().toISOString()
   });
 
